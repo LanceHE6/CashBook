2aff808b05f81a13cd86d5d8bd34d8b804dcf71b
LanceHE6 <2765543491@qq.com>
2023-06-07 10:50:02 +0800

    基本实现所有功能

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,10 @@
 package org.example;
 
-
-import javax.swing.*;
 import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         new Home();
     }
 }
