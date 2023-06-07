e8b934db6d2863b67fe9707e750ac57e3aa3e198
LanceHE6 <2765543491@qq.com>
2023-06-07 10:48:43 +0800

    改为记录类

--- src/main/java/org/example/BillingRecord.java
+++ src/main/java/org/example/BillingRecord.java
@@ -8,54 +8,14 @@ import java.util.Date;
 /**
  * 账单记录数据结构类
  * 实现Comparable接口
+ *
+ * @param type 1为收入，0为支出
  */
-public class BillingRecord implements Comparable<BillingRecord>{
-    private String name;
-    private String date;
-    private double amount;
-    private int type;//1为收入，0为支出
-
-    public BillingRecord(String name, String date, double amount, int type){
-        this.name = name;
-        this.date = date;
-        this.amount = amount;
-        this.type = type;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public void setType(int type) {
-        this.type = type;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public int getType() {
-        return type;
-    }
+public record BillingRecord(String name, String date, double amount, int type) implements Comparable<BillingRecord> {
 
     /**
-     * 重写比较方法
+     * 重写比较接口
+     *
      * @param other the object to be compared.
      * @return 0
      */
@@ -71,6 +31,7 @@ public class BillingRecord implements Comparable<BillingRecord>{
         }
         return 0;
     }
+
     public String getYearMonth() {
         DateFormat inputFormat = new SimpleDateFormat("yy/MM/dd");
         DateFormat outputFormat = new SimpleDateFormat("yy-MM");
