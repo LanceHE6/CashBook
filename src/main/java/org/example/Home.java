6b9f91b9f9d639e077c7a7f264689e0fc6ccc4b4
LanceHE6 <2765543491@qq.com>
2023-06-07 10:49:33 +0800

    基本实现所有功能

--- src/main/java/org/example/Home.java
+++ src/main/java/org/example/Home.java
@@ -1,23 +1,26 @@
 package org.example;
 
 import jxl.Workbook;
+import jxl.Sheet;
+import jxl.Cell;
+import jxl.read.biff.BiffException;
 import jxl.write.Label;
 import jxl.write.WritableSheet;
 import jxl.write.WritableWorkbook;
 
 import javax.swing.*;
-import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+import javax.swing.filechooser.FileFilter;
 import javax.swing.table.DefaultTableModel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.List;
-
 
 /**
  *  主界面 继承JFrame
@@ -51,11 +54,15 @@ public class Home extends JFrame {
     //数据库连接
     private final SqliteManager sqliteManager = new SqliteManager( System.getProperty("user.dir") + "/data/data.db");  // 数据库文件的相对路径);
 
-    public Home() throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+    public Home() throws ClassNotFoundException, SQLException {
         super("记账本");//调用父类构造函数
         Utils utils = new Utils();//工具类
         int windowWidth=800;
-        int widowHeight=525;
+        int widowHeight=550;
+        //图标
+        ImageIcon imageIcon = new ImageIcon(System.getProperty("user.dir") + "/data/homeIcon.png");
+        setIconImage(imageIcon.getImage().getScaledInstance(80, 80, Image.SCALE_DEFAULT));
+
         //数据库建表
         String createTable = "CREATE TABLE IF NOT EXISTS CashBook ( no  INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, name TEXT, amount REAL, type INTEGER);";
         sqliteManager.executeUpdate(createTable);
@@ -64,6 +71,30 @@ public class Home extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(400,300,windowWidth,widowHeight);
         setResizable(false);//不可拉伸窗口
+
+        //菜单栏
+        JMenuBar jMenuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("文件");
+        JMenu windowMenu = new JMenu("窗口");
+        jMenuBar.add(fileMenu);
+        jMenuBar.add(windowMenu);
+        fileMenu.setFont(new Font("simhei",Font.BOLD,14));
+        windowMenu.setFont(new Font("simhei",Font.BOLD,14));
+        JMenuItem exportExcelAll = new JMenuItem("导出全部为Excel文件");
+        exportExcelAll.setFont(new Font("simhei",Font.PLAIN,14));
+        JMenuItem exportExcelSelected = new JMenuItem("导出选中为Excel文件");
+        exportExcelSelected.setFont(new Font("simhei",Font.PLAIN,14));
+        JMenuItem importExcel = new JMenuItem("从Excel文件导入");
+        importExcel.setFont(new Font("simhei",Font.PLAIN,14));
+        JMenuItem chart = new JMenuItem("查看图表");
+        chart.setFont(new Font("simhei",Font.PLAIN,14));
+
+        fileMenu.add(importExcel);
+        fileMenu.add(exportExcelAll);
+        fileMenu.add(exportExcelSelected);
+        windowMenu.add(chart);
+
+        setJMenuBar(jMenuBar);
 
         //创建各种组件
         JPanel panel = new JPanel(null);//总容器 绝对布局
@@ -76,42 +107,56 @@ public class Home extends JFrame {
         List<BillingRecord> billingRecords = sqliteManager.queryBillingRecords();
         //依据日期排序
         Collections.sort(billingRecords);
-        //填充账单信息
+        //填充表格账单信息
         for (BillingRecord record : billingRecords) {
-            Object[] rowData = {record.getDate(), record.getName(), record.getAmount(), record.getType()==1?"收入":"支出"};
+            Object[] rowData = {record.date(), record.name(), record.amount(), record.type()==1?"收入":"支出"};
             tableModel.addRow(rowData);
         }
         //依据表头创建表格
         MyTable table = new MyTable(tableModel);
-        //表格能够滑动
+
+        //滑动表格
         JScrollPane scrollPane = new JScrollPane(table);
         //设置表头不能移动
         table.getTableHeader().setReorderingAllowed(false);
-        scrollPane.setBounds(20,40,windowWidth-50,widowHeight-175);
+        scrollPane.setBounds(20,40,windowWidth-50,widowHeight-200);
         panel.add(scrollPane);
 
+        //筛选框
+        JLabel filterProject = new JLabel("筛选:");
+        filterProject.setBounds(20,0,50,35);
+        panel.add(filterProject);
+
+        JTableFilterTextField jTableFilterTextField = new JTableFilterTextField();
+        jTableFilterTextField.setTable(table);
+        jTableFilterTextField.setBounds(55,5,100,24);
+        panel.add(jTableFilterTextField);
+
+        //总收入
         JLabel totalInProject = new JLabel("总收入：");
         totalInProject.setBounds(220,400,50,35);
         panel.add(totalInProject);
-        //总收入
+
         double totalIncome = sqliteManager.getTotalAmount(1);
         lblTotalIncome = new JLabel(""+ totalIncome+"元");
         lblTotalIncome.setBounds(270,400,100,35);
         panel.add(lblTotalIncome);
 
+        //总支出
         JLabel totalExProject = new JLabel("总支出：");
         totalExProject.setBounds(360,400,50,35);
         panel.add(totalExProject);
-        //总支出
+
         double totalExpense = sqliteManager.getTotalAmount(0);
         lblTotalExpense = new JLabel(""+totalExpense+"元");
         lblTotalExpense.setBounds(410,400,100,35);
         panel.add(lblTotalExpense);
 
+        //结余
         JLabel balanceProject = new JLabel("结余：");
         balanceProject.setBounds(490,400,40,35);
         panel.add(balanceProject);
-        //结余
+
         lblBalance = new JLabel(""+(totalIncome-totalExpense)+"元");
         lblBalance.setBounds(530,400,100,35);
         panel.add(lblBalance);
@@ -160,21 +205,14 @@ public class Home extends JFrame {
         panel.add(btnSubmit);
 
         JButton btnDelete = new JButton("删除项目");
-        btnDelete.setBounds(20,5,100,30);
+        btnDelete.setBounds(668,5,100,30);
         panel.add(btnDelete);
 
-        JButton btnExport = new JButton("导出Excel文件");
-        btnExport.setBounds(windowWidth-180,5,150,30);
-        panel.add(btnExport);
-
-        JButton btnChart = new JButton("查看图表");
-        btnChart.setBounds(windowWidth-300,5,100,30);
-        panel.add(btnChart);
 
         add(panel);
 
         setVisible(true);
-        UIManager.setLookAndFeel(new NimbusLookAndFeel());//主題
+        //UIManager.setLookAndFeel(new NimbusLookAndFeel());//主題
 
         //添加事件监听器
         btnSubmit.addActionListener(e -> {
@@ -223,7 +261,6 @@ public class Home extends JFrame {
                // 刷新表格数据
                 refreshData();
 
-                //JOptionPane.showMessageDialog(null, "项目提交成功");
             } catch (NumberFormatException | SQLException | ClassNotFoundException ex) {
                 JOptionPane.showMessageDialog(null, "提交项目时出错: " + ex.getMessage());
             }
@@ -249,7 +286,7 @@ public class Home extends JFrame {
                 }
                 try {
 
-                    int userChoose = JOptionPane.showConfirmDialog(null,"是否删除选中的行");
+                    int userChoose = JOptionPane.showConfirmDialog(null,"是否删除选中的行","删除项目",JOptionPane.YES_NO_OPTION);
                     if(userChoose == JOptionPane.YES_OPTION){
                         sqliteManager.deleteRecords(deleteRecords);
                     }else {
@@ -275,7 +312,7 @@ public class Home extends JFrame {
             int type = table.getValueAt(index1,3)=="收入"?1:0;
             BillingRecord deleteRecord = new BillingRecord(name, date, amount, type);
 
-            int userChoose = JOptionPane.showConfirmDialog(null,"是否删除选中的行");
+            int userChoose = JOptionPane.showConfirmDialog(null,"是否删除选中的行","删除项目",JOptionPane.YES_NO_OPTION);
             if(userChoose == JOptionPane.YES_OPTION){
                 try {
                     sqliteManager.deleteRecord(deleteRecord);
@@ -294,32 +331,106 @@ public class Home extends JFrame {
 
         });
 
-        btnExport.addActionListener(e -> {
+        exportExcelAll.addActionListener(e -> {
             JFileChooser fileChooser = new JFileChooser();
             fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
             int result = fileChooser.showDialog(null,"选择为Excel保存路径");
             if(result == JFileChooser.APPROVE_OPTION) {
                 String savePath = String.valueOf(fileChooser.getSelectedFile());
-                writeExcel(savePath);
+                try {
+                    writeExcel(savePath, sqliteManager.queryBillingRecords());
+                } catch (SQLException | ClassNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                }
             }
         });
 
-        btnChart.addActionListener(new ActionListener() {
-          @Override
-          public void actionPerformed(ActionEvent e) {
-              try {
-                  new ChartFrame(sqliteManager.queryBillingRecords());
-              } catch (SQLException | ClassNotFoundException ex) {
-                  throw new RuntimeException(ex);
-              }
-          }
-      });
+        exportExcelSelected.addActionListener(e -> {
+            int[] index = table.getSelectedRows();//获取选中的行
+            //如果选中多行
+            if (index.length == 0) {
+                JOptionPane.showMessageDialog(null, "请选中需要导出的行");
+            }else {
+                List<BillingRecord> exportRecords = new ArrayList<>();
+                for (int row : index) {
+                    String name = (String) table.getValueAt(row, 1);
+                    String date = (String) table.getValueAt(row, 0);
+                    double amount = (double) table.getValueAt(row, 2);
+                    int type = table.getValueAt(row, 3) == "收入" ? 1 : 0;
+                    BillingRecord record = new BillingRecord(name, date, amount, type);
+                    exportRecords.add(record);
+                }
+                //路径选择
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                int result = fileChooser.showDialog(null,"选择为Excel保存路径");
+                if(result == JFileChooser.APPROVE_OPTION) {
+                    String savePath = String.valueOf(fileChooser.getSelectedFile());
+                    writeExcel(savePath, exportRecords);
+                }
+            }
+        });
+
+        chart.addActionListener(e -> {
+            try {
+                new ChartFrame(sqliteManager.queryBillingRecords());
+            } catch (SQLException | ClassNotFoundException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        importExcel.addActionListener(e -> {
+            //路径选择
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            //设置文件过滤器
+            fileChooser.setFileFilter(new FileFilter() {
+                @Override
+                public boolean accept(File f) {
+                    return f.getName().toLowerCase().endsWith(".xls");
+                }
+                @Override
+                public String getDescription() {
+                    return "Excel文件(*.xls)";
+                }
+            });
+            int result = fileChooser.showDialog(null,"选择Excel文件");
+            if(result == JFileChooser.APPROVE_OPTION) {
+                String file = String.valueOf(fileChooser.getSelectedFile());
+                try {
+                    List<BillingRecord> exportRecords = new ArrayList<>();
+                    FileInputStream fis = new FileInputStream(file);
+                    //StringBuilder sb = new StringBuilder();
+                    Workbook rwb = Workbook.getWorkbook(fis);
+                    Sheet rs = rwb.getSheet(0);
+                    for (int i = 1; i < rs.getRows(); i++) {
+                        Cell[] cells = rs.getRow(i);
+
+                        String date = cells[0].getContents();
+                        String name = cells[1].getContents();
+                        double amount = Double.parseDouble(cells[2].getContents());
+                        int type = Objects.equals(cells[3].getContents(), "收入") ? 1 : 0;
+
+                        exportRecords.add(new BillingRecord(name, date, amount, type));
+
+                    }
+                    sqliteManager.insertBillingRecords(exportRecords);
+                } catch (IOException | BiffException | SQLException ex) {
+                    JOptionPane.showMessageDialog(null, "导入失败:\n%s".formatted(ex));
+                    throw new RuntimeException(ex);
+                }
+                JOptionPane.showMessageDialog(null, "导入成功");
+                try {
+                    refreshData();//刷新
+                } catch (SQLException | ClassNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
     }
 
     /**
      * 刷新数据
-     * @throws SQLException
-     * @throws ClassNotFoundException
      */
     private void refreshData() throws SQLException, ClassNotFoundException {
         //清空表格数据
@@ -330,7 +441,7 @@ public class Home extends JFrame {
         Collections.sort(billingRecords);
         //填充账单信息
         for (BillingRecord record : billingRecords) {
-            Object[] rowData = {record.getDate(), record.getName(), record.getAmount(), record.getType()==1?"收入":"支出"};
+            Object[] rowData = {record.date(), record.name(), record.amount(), record.type()==1?"收入":"支出"};
             tableModel.addRow(rowData);
         }
 
@@ -348,7 +459,7 @@ public class Home extends JFrame {
      * 写入Excel文件
      * @param savePath 文件保存路径
      */
-    private void writeExcel(String savePath){
+    private void writeExcel(String savePath, List<BillingRecord> records){
         //开始写入excel,创建模型文件头
         String[] titleA = {"日期","项目","金额","类型"};
         //创建Excel文件，B库CD表文件
@@ -357,31 +468,31 @@ public class Home extends JFrame {
         System.out.println(savePath+"/"+nowTime+".xls");
         if(fileA.exists()){
             //如果文件存在就删除
+            //noinspection ResultOfMethodCallIgnored
             fileA.delete();
         }
         try {
+            //noinspection ResultOfMethodCallIgnored
             fileA.createNewFile();
             //创建工作簿
             WritableWorkbook workbookA = Workbook.createWorkbook(fileA);
             //创建sheet
             WritableSheet sheetA = workbookA.createSheet("sheet1", 0);
-            Label labelA = null;
+            Label labelA;
             //设置列名
             for (int i = 0; i < titleA.length; i++) {
                 labelA = new Label(i,0,titleA[i]);
                 sheetA.addCell(labelA);
             }
-            //获取数据源
-            List<BillingRecord> records = sqliteManager.queryBillingRecords();
             int column = 1;
             for (BillingRecord record: records) {
-                labelA = new Label(0,column,record.getDate());
+                labelA = new Label(0,column,record.date());
                 sheetA.addCell(labelA);
-                labelA = new Label(1,column,record.getName());
+                labelA = new Label(1,column,record.name());
                 sheetA.addCell(labelA);
-                labelA = new Label(2,column,String.valueOf(record.getAmount()));
+                labelA = new Label(2,column,String.valueOf(record.amount()));
                 sheetA.addCell(labelA);
-                labelA = new Label(3,column,record.getType() == 1? "收入": "支出");
+                labelA = new Label(3,column,record.type() == 1? "收入": "支出");
                 sheetA.addCell(labelA);
                 column ++;
             }
@@ -395,4 +506,3 @@ public class Home extends JFrame {
         }
     }
 }
-
